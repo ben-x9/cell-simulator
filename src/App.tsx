42c72e29e6756c10dae4938d01436fc0cae7c30c
Benjamin James <ben.jx9@gmail.com>
2022-08-13 14:57:18 +0900

    Extract BoardView

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,17 @@
-import { useState } from "react"
 import "./App.css"
-
-import clsx from "clsx"
+import { useState, SetStateAction, Dispatch } from "react"
 import { Board, generateBoard, newBoard, toggleCell } from "./models/board"
+import clsx from "clsx"
 
 const initialBoard = newBoard(32)
 
 export default function App() {
   const [board, setBoard] = useState<Board>(initialBoard)
-  const toggle = (r: number, c: number) => setBoard(toggleCell(board, r, c))
   const generate = () => setBoard(generateBoard)
 
   return (
     <div className="App">
-      <table>
-        <tbody>
-          {board.map((row, r) => (
-            <tr key={r}>
-              {row.map((cell, c) => (
-                <td
-                  key={c}
-                  className={clsx("cell", { on: cell })}
-                  onMouseDown={() => toggle(r, c)}
-                ></td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <BoardView board={board} setBoard={setBoard} />
       <div className="controls">
         <span className="button" onClick={() => setBoard(initialBoard)}>
           Reset
@@ -35,5 +19,32 @@ export default function App() {
         <button onClick={generate}>Next Generation</button>
       </div>
     </div>
+  )
+}
+
+export const BoardView = ({
+  board,
+  setBoard,
+}: {
+  board: Board
+  setBoard: Dispatch<SetStateAction<Board>>
+}) => {
+  const toggle = (r: number, c: number) => setBoard(toggleCell(board, r, c))
+  return (
+    <table>
+      <tbody>
+        {board.map((row, r) => (
+          <tr key={r}>
+            {row.map((cell, c) => (
+              <td
+                key={c}
+                className={clsx("cell", { on: cell })}
+                onMouseDown={() => toggle(r, c)}
+              ></td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
   )
 }
